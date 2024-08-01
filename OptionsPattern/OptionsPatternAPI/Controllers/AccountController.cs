062046f02f69200349b1d3b2a33435ab6af0a59f
farooqhashmi13 <120180306+farooqhashmi13@users.noreply.github.com>
2024-08-01 21:13:24 +0500

    initial code

---
+++ OptionsPattern/OptionsPatternAPI/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace OptionsPatternAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly KeyVault _keyVault;
+        public AccountController(IOptions<KeyVault> keyVault)
+        {
+            _keyVault = keyVault.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_keyVault);
+        }
+    }
+}
---
+++ OptionsPattern/OptionsPatternAPI/KeyVault.cs
@@ -0,0 +1,10 @@
+﻿namespace OptionsPatternAPI
+{
+    public class KeyVault
+    {
+        public string? KeyVaultURL { get; set; }
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+        public string? DirectoryId { get; set; }
+    }
+}
